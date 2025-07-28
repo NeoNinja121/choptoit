aa0ac3627d52869abf322480a1248558acb29643
NeoNinja121 <markbassett1@gmail.com>
2025-07-28 09:44:10 +0100

    Adjust version format and NPC size

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # Update VERSION constant in index.html based on git commit count
 count=$(git rev-list --count HEAD)
-# Format version as v<count>
-sed -i -E "s/const VERSION = 'v[0-9.]+';/const VERSION = 'v$count';/" index.html
+major=$((count / 100))
+minor=$((count % 100))
+version="v${major}.${minor}"
+sed -i -E "s/const VERSION = 'v[0-9.]+';/const VERSION = '$version';/" index.html
